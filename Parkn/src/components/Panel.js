ca63cdbdd248b89cc3327ece55cdb1be05c629c1
jaredramirez <jaredramirez@me.com>
2016-12-06 00:53:27 -0600

    added snap to positions to control panel

--- Parkn/src/components/Panel.js
+++ Parkn/src/components/Panel.js
@@ -12,11 +12,21 @@ import Bar from './Bar';
 
 const { width, height } = Dimensions.get('window');
 
+// used for animation
+// basis for overlay positioning
+const percentOf = (total: number, percentage: number) => {
+	return (percentage < 0) ? 0 : total * (percentage / 100);
+}
+
 class Panel extends Component {
+	state:  {
+		pan: Object
+	};
+
 	constructor(props) {
 		super(props);
 		this.state = {
-			pan: new Animated.ValueXY()
+			pan: new Animated.ValueXY(),
 		};
 	}
 
@@ -36,31 +46,43 @@ class Panel extends Component {
 				null, {dy: this.state.pan.y}
 			]), // handle movement and offset, possible to have a `dx` as well to allow for horizontal movment
 			//TODO: create system to bounce back if pull to offscreen areas
-			onPanResponderRelease: () => {
-				//this.state.pan.flattenOffset(); // ?? Flatten the offset so it resets the default positioning
+			onPanResponderRelease: (e, {vx, vy}) => {
+				this.state.pan.flattenOffset(); // ?? Flatten the offset so it resets the default positioning
+
+				Animated.decay(this.state.pan, {
+					velocity: {x: vx, y: vy},
+					deceleration: 0.98 
+				}).start();
+
+				let y = this.state.pan.y._value,
+					position = 0;
+
+				if(y < -percentOf(height, 60))
+					position = -percentOf(height, 85);
+
 				Animated.spring(this.state.pan, {
-					toValue: 0
+					toValue: {x: 0, y: position},
 				}).start();
 			}
 		});
 	}
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<View style={[styles.view, styles.dimensions]}>
-					{this.props.children}
-				</View>
-				<Animated.View
-					style={this.getOverlayStyle()}
-					{...this._panResponder.panHandlers}
-				>
-					<View>
-						<Bar />
+		render() {
+			return (
+				<View style={styles.container}>
+					<View style={[styles.view, styles.dimensions]}>
+						{this.props.children}
 					</View>
-				</Animated.View>
-			</View>
-		);
+					<Animated.View
+						style={this.getOverlayStyle()}
+						{...this._panResponder.panHandlers}
+					>
+						<View>
+							<Bar />
+						</View>
+					</Animated.View>
+				</View>
+			);
 	}
 
 	componentWillUnmount() {
@@ -94,7 +116,7 @@ const styles = {
 		position: 'absolute',
 	},
 	overlay: {
-		top: height-75,
+		top: percentOf(height, 90),
 	},
 };
 
